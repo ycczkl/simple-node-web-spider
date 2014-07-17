126f2f9f0e649809758a96132d60457921e1edb0
ycczkl <ycczkl91@gmail.com>
2014-07-16 23:36:58 -0400

    first commit

---
+++ login.js
@@ -0,0 +1,22 @@
+/**
+ * Created by LIU on 7/3/14.
+ */
+var request = require('request');
+var r = request.post('http://profitsi.com/clockin/timeclock.php', function optionalCallback (err, httpResponse, body) {
+    if (err) {
+        return console.error('upload failed:', err);
+    }
+    console.log('Successful');
+});
+console.log(process.argv[2]);
+var form = r.form();
+form.append('left_fullname', 'Kelun Zhang');
+form.append('employee_passwd', 'welcomekelun');
+form.append('left_inout', process.argv[2]);
+form.append('submit_button', 'Punch Status');
+
+//left_fullname	Kelun Zhang
+//employee_passwd	welcomekelun
+//left_inout	out
+//left_notes
+//submit_button	Punch Status
--- spider.js
+++ spider.js
@@ -2,21 +2,31 @@
  * Created by Kelun on 7/2/14.
  */
 //use request to set up http request
-var request = require("request");
+var request = require("request")
 //use cheerio to deal with data
-var cheerio = require("cheerio");
+    ,cheerio = require("cheerio")
+    ,iconv = require('iconv-lite');
 
-request("http://www.fifa.com/worldcup/teams/index.html", function (error, response, body) {
+var options = {
+    encoding: null,
+    url: 'http://tieba.baidu.com/f?kw=%B1%B1%BE%A9%B9%FA%B0%B2&fr=index'
+};
+request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         var arr = [];
         //load data
-        var $ = cheerio.load(body);
+
+        var result = iconv.decode(body, 'GBK');
+        var $ = cheerio.load(result);
         /* 1.find all element which class name is 'team-name'
            2.Iterate these elements, and save the text inside the element into an array
            3.$(this) is equal to elm
          */
-        $('.team-name').each(function(i, elm){
-            arr[i] = $(this).text();
+
+        //console.log(result);
+        $('.j_th_tit').each(function(i, elm){
+            var title = $(this).text();
+            arr[i] = title;
         });
         console.log(arr);
     }
